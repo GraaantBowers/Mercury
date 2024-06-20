b4af5c2ee81393984f3e9a61e21d2b75c5f514d0
Graaant <grant_bowers@icloud.com>
2024-06-20 08:50:41 -0500

    Commit 2024-06-20T08:50:41

---
+++ commit.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+# Navigate to your repository directory
+cd ~/sites/wiki/
+
+# Add all changes
+git add .
+
+# Get the current date and time in the required format
+DATE=$(date +"%Y-%m-%dT%H:%M:%S")
+
+# Commit with the date in the message
+git commit -m "Commit $DATE"
+
+# Push to the main branch
+git push -u origin main
